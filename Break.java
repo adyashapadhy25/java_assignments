e712056ab07f156da8690e846d099a50b76dee6f
adyashapadhy25 <adyashapadhy05@gmail.com>
2025-02-16 15:41:16 +0530

    Add files via upload
    "Break statement in java terminates the execution of a loop or switch block".

---
+++ Break.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class Break {
+
+    public static void main (String[] args) {
+
+      //assigning n as integer value
+      int n = 1;
+
+      //passing n to switch
+      // it will check n and display output accordingly
+      switch(n) {
+
+        case 1:
+          System.out.println("Adyasha padhy");
+          break;
+        case 2:
+          System.out.println("Second Case");
+          break;
+        default:
+          System.out.println("default case");
+      }
+    }
+}
